3742620c850fc9eedfe910c599097885a3c398a8
caoa14396 <93616309+caoa14396@users.noreply.github.com>
2021-11-02 14:50:57 -0500

    Update Carlos-Oliveros-Roomba.js

--- Carlos-Oliveros-Roomba.js
+++ Carlos-Oliveros-Roomba.js
@@ -2,6 +2,7 @@
 -- Takes in input file of coordinates. Marks grid limit, Roomba starting position, and takes in location of trash
     through (X,Y) coordinates
     ** IMPORTANT ** to insert Test file, MUST change File Path in line: var array = fs.readFileSync"...." line: 134
+    ***RUN IN TERMINAL - JS Node
 -- Output:
     - Roomba starting position
     - Trash positions
